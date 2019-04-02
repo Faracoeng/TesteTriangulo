2267814d3a0edc33e12f9ef6d304fe649bdf06d6
Faracoeng <alfmazucheli>
2019-04-02 14:31:44 -0300

    Compilando e testes aplicados

--- src/main/java/poo/Triangulo.java
+++ src/main/java/poo/Triangulo.java
@@ -4,25 +4,36 @@ public class Triangulo {
     private int aux = 0;
     public String tipo(int a, int b, int c){
 
+        //--------------------------------------------
+        // Verificando se é um triângulo
+
         if(a <=0  || b <=0 || c <= 0){
             return "Não é um triângulo";
         }
 
-        if (a > (b + c)){
+        else if (a > (b + c)){
                 return "Não é um triângulo";
+        }
+        else if (b > (c + a)){
+            return "Não é um triângulo";
+        }
+        else if (c > (a + b)){
+            return "Não é um triângulo";
         }
 
 
-       //------------------------------------------
+        //------------------------------------------------
+        //Equilátero e Escaleno
 
-        if(a == b && b == c){
+        else if(a == b && b == c){
             return "É um triângulo Equilátero";
         }
 
         else if(a != b && b != c && c !=a ){
             return "É um triângulo Escaleno";
         }
-        //-----------------------------------------
+        //-------------------------------------------------
+        //Se não for nenhum dos anteriores será  Isósceles
 
         return "É um triângulo Isósceles";
 
--- src/test/java/poo/TestaTriangulo.java
+++ src/test/java/poo/TestaTriangulo.java
@@ -9,10 +9,15 @@ public class TestaTriangulo {
     public void testarTriangulo(){
         Triangulo tri = new Triangulo();
         Assert.assertEquals("0x0x0 não é um triângulo","Não é um triângulo", tri.tipo(0,0,0));
+        Assert.assertEquals("3x4x8 não é um triângulo","Não é um triângulo", tri.tipo(3,4,8));
         Assert.assertEquals("10x1x2 não é um triângulo","Não é um triângulo", tri.tipo(10,1,2));
         Assert.assertEquals("1x10x2 não é um triângulo","Não é um triângulo", tri.tipo(1,10,2));
-        Assert.assertEquals("1x10x2 não é um triângulo","Não é um triângulo", tri.tipo(2,10,1));
+        Assert.assertEquals("2x10x1 não é um triângulo","Não é um triângulo", tri.tipo(2,10,1));
+        Assert.assertEquals("-1x5x5 não é um triângulo","Não é um triângulo", tri.tipo(-1,5,5));
+        Assert.assertEquals("4x7x6 não é um triângulo","É um triângulo Escaleno", tri.tipo(4,7,6));
+
         Assert.assertEquals("6x6x6 é um triângulo equilátero","É um triângulo Equilátero",tri.tipo(6,6,6));
+
         Assert.assertEquals("6x4x4 é um triângulo Isósceles","É um triângulo Isósceles",tri.tipo(6,4,4));
         Assert.assertEquals("4x4x6 é um triângulo Isósceles","É um triângulo Isósceles",tri.tipo(4,4,6));
         Assert.assertEquals("4x4x6 é um triângulo Isósceles","É um triângulo Isósceles",tri.tipo(4,6,4));
