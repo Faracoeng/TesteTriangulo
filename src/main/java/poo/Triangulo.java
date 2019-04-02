df91b8c20d73085c07ab6b058eaaac695c59c50c
Faracoeng <alfmazucheli>
2019-04-02 13:46:27 -0300

    testes iniciados (compilando)

--- src/main/java/poo/Triangulo.java
+++ src/main/java/poo/Triangulo.java
@@ -3,7 +3,10 @@ package poo;
 public class Triangulo {
     public String tipo(int a, int b, int c){
 
-        if(a == b && b == c){
+        if(a == 0 && b == 0 && c == 0){
+            return "Não é um triângulo";
+        }
+        else if(a == b && b == c){
             return "É um triângulo Equilátero";
         }
 
--- src/test/java/poo/TestaTriangulo.java
+++ src/test/java/poo/TestaTriangulo.java
@@ -8,12 +8,15 @@ public class TestaTriangulo {
     @Test
     public void testarTriangulo(){
         Triangulo tri = new Triangulo();
-        //Assert.assertEquals("10x1x2 não é um triângulo","Não é um triângulo", tri.tipo(10,1,2));
+        Assert.assertEquals("0x0x0 não é um triângulo","Não é um triângulo", tri.tipo(0,0,0));
+        Assert.assertEquals("10x1x2 não é um triângulo","Não é um triângulo", tri.tipo(10,1,2));
+        Assert.assertEquals("1x10x2 não é um triângulo","Não é um triângulo", tri.tipo(1,10,2));
+        Assert.assertEquals("1x10x2 não é um triângulo","Não é um triângulo", tri.tipo(2,10,1));
         Assert.assertEquals("6x6x6 é um triângulo equilátero","É um triângulo Equilátero",tri.tipo(6,6,6));
-        //Assert.assertEquals("6x4x4 é um triângulo Isósceles","É um triângulo Isósceles",tri.tipo(6,4,4));
-        //Assert.assertEquals("4x4x6 é um triângulo Isósceles","É um triângulo Isósceles",tri.tipo(4,4,6));
-        //Assert.assertEquals("4x4x6 é um triângulo Isósceles","É um triângulo Isósceles",tri.tipo(4,6,4));
-        //Assert.assertEquals("-6x6x6 não é um triângulo","Não insira números negativos",tri.tipo(-6,6,4));
+        Assert.assertEquals("6x4x4 é um triângulo Isósceles","É um triângulo Isósceles",tri.tipo(6,4,4));
+        Assert.assertEquals("4x4x6 é um triângulo Isósceles","É um triângulo Isósceles",tri.tipo(4,4,6));
+        Assert.assertEquals("4x4x6 é um triângulo Isósceles","É um triângulo Isósceles",tri.tipo(4,6,4));
+
 
     }
 
