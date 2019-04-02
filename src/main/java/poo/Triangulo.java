abb805c4b66c00bbf49c5e29733574e7c205318f
Faracoeng <alfmazucheli>
2019-04-02 14:11:17 -0300

    testes iniciados (ajustando) (compilando)

--- src/main/java/poo/Triangulo.java
+++ src/main/java/poo/Triangulo.java
@@ -1,27 +1,30 @@
 package poo;
 
 public class Triangulo {
+    private int aux = 0;
     public String tipo(int a, int b, int c){
 
-        if(a == 0 && b == 0 && c == 0){
+        if(a <=0  || b <=0 || c <= 0){
             return "Não é um triângulo";
         }
-        else if(a == b && b == c){
+
+        if (a > (b + c)){
+                return "Não é um triângulo";
+        }
+
+
+       //------------------------------------------
+
+        if(a == b && b == c){
             return "É um triângulo Equilátero";
         }
 
-        else if (a != b && b == c) {
-            return "É um triângulo Isósceles";  // erro pode estar aq
+        else if(a != b && b != c && c !=a ){
+            return "É um triângulo Escaleno";
         }
+        //-----------------------------------------
 
-        else if (a == b && a != c) {
-            return "É um triângulo Isósceles";
-        }
+        return "É um triângulo Isósceles";
 
-        if (a == c && b != c) {
-            return "É um triângulo Isósceles";
-        }
-
-        return "Não é um triângulo";
     }
 }
